9cd5a2ae808f70a2127100e659bc191559111685
Pieter Bikkel <pieter@bikkel.pro>
2023-10-11 15:43:52 +0200

    Initial Commit

---
+++ VolleyStats/VolleyStatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  VolleyStatsApp.swift
+//  VolleyStats
+//
+//  Created by Pieter Bikkel on 11/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct VolleyStatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
